5003d16c12fe8265b05c8691f35a6f38c0121d9f
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2017-07-08 21:08:40 +0200

    Add support for stacker deployment * modify entryscript to handle secrets if present * add new stack compose file for stack deployment

--- Dockerfile
+++ Dockerfile
@@ -43,10 +43,14 @@ RUN \
 
 # add tomcat config
 COPY conf/tomcat-users.xml /opt/apache-tomcat/conf/tomcat-users.xml
+COPY conf/tomcat-users-template.xml /opt/apache-tomcat/conf/tomcat-users-template.xml
+
 COPY docker-entrypoint.sh /
 
 RUN \
-  chown "${TOMCAT_USER}":"${TOMCAT_USER}" /docker-entrypoint.sh && \
+  chown "${TOMCAT_USER}":"${TOMCAT_USER}" /opt/apache-tomcat/conf/tomcat-users.xml; \
+  chown "${TOMCAT_USER}":"${TOMCAT_USER}" /opt/apache-tomcat/conf/tomcat-users-template.xml; \
+  chown "${TOMCAT_USER}":"${TOMCAT_USER}" /docker-entrypoint.sh; \
   chmod 755 /docker-entrypoint.sh
 
 EXPOSE 8080
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,31 @@
 # abort when trying to use unset variable
 set -o nounset
 
-exec su-exec tomcat ${CATALINA_HOME}/bin/catalina.sh run
+tomcat_home="/home/tomcat"
+tomcat_template_user_config="/opt/apache-tomcat/conf/tomcat-users-template.xml"
+tomcat_user_config="/opt/apache-tomcat/conf/tomcat-users.xml"
+tomcat_app_user="/run/secrets/com.ragedunicorn.tomcat.app_user"
+tomcat_app_user_password="/run/secrets/com.ragedunicorn.tomcat.app_user_password"
+
+# if secret is set update configuration accordingly
+if [ -f "${tomcat_app_user}" ] && [ -f "${tomcat_app_user_password}" ]; then
+  # track whether init process was already done
+  if [ ! -f "${tomcat_home}/.init" ]; then
+    echo "$(date) [INFO]: Overriding tomcat configuration"
+
+    cp "${tomcat_template_user_config}" "${tomcat_template_user_config}.tmp"
+    sed -i -e "s/\\[username\\]/$(cat ${tomcat_app_user})/" "${tomcat_template_user_config}.tmp"
+    sed -i -e "s/\\[password\\]/$(cat ${tomcat_app_user_password})/" "${tomcat_template_user_config}.tmp"
+    sed -i -e "s/\\[roles\\]/admin-gui,admin-script,manager-gui,manager-script,manager-jmx/" "${tomcat_template_user_config}.tmp"
+
+    mv "${tomcat_template_user_config}.tmp" "${tomcat_user_config}"
+    rm "${tomcat_template_user_config}.tmp"
+
+    touch "${tomcat_home}/.init"
+    echo "$(date) [INFO]: Successfully updated tomcat configuration"
+  fi
+else
+  echo "$(date) [INFO]: Using default tomcat users configuration"
+fi
+
+exec su-exec "${TOMCAT_USER}" "${CATALINA_HOME}/bin/catalina.sh" run
